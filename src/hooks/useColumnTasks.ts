1e1026f3b11805cd4649708ded262e9901ab6069
luiscaputo <luiscaputo15@gmail.com>
2022-12-05 19:15:23 +0100

    fix: Remove unnecessary field or function

--- src/hooks/useColumnTasks.ts
+++ src/hooks/useColumnTasks.ts
@@ -1,7 +1,7 @@
 import { useCallback } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { ColumnType } from '../utils/enums';
-import { pickChakraRandomColor, swap } from '../utils/helpers';
+import { swap } from '../utils/helpers';
 import { debug } from '../utils/logging';
 import { TaskModel } from '../utils/models';
 import useTaskCollection from './useTaskCollection';
