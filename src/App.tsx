6e8a53eabf6cd5c1da7bf934085fcbf89b760295
luiscaputo <luiscaputo15@gmail.com>
2022-12-05 18:54:03 +0100

    feat: Default init application file

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import {} from '@chakra-ui/icons';
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import Column from './components/Column';
+// import Column from './components/Column';
+import { ColumnType } from './utils/enums';
+
+function App() {
+  return (
+    <main>
+      <Heading
+        fontSize={{ base: '4xl', sm: '5xl', md: '6xl' }}
+        fontWeight="bold"
+        textAlign="center"
+        bgGradient="linear(to-l, #FF0080, #FFA500)"
+        bgClip="text"
+        mt={4}
+      >
+        Mamboo Frontend Challenge
+
+      </Heading>
+      <DndProvider backend={HTML5Backend}>
+        <Container maxWidth="container.lg" px={4} py={10}>
+          <SimpleGrid
+            columns={{ base: 1, md: 4 }}
+            spacing={{ base: 16, md: 4 }}
+          >
+            <Column column={ColumnType.TO_DO} />
+            <Column column={ColumnType.IN_PROGRESS} />
+            <Column column={ColumnType.BLOCKED} />
+            <Column column={ColumnType.COMPLETED} />
+          </SimpleGrid>
+        </Container>
+      </DndProvider>
+    </main>
+  );
+}
+export default App;
